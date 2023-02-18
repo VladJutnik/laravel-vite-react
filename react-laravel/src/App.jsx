04dd89caf5952aad3d634c5353ae4c1b34ada567
VA <v2517v@gmail.com>
2023-02-18 22:05:14 +0700

    start project

---
+++ react-laravel/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline bg-p">
+      <BeakerIcon className="h-6 w-6 text-blue-500"/>
+      <p>App.jsx</p>
+    </div>
+  )
+}
+
+export default App

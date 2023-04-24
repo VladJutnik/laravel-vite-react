76f1d04a88ed366003eccce8d502b393104e3bae
VA <v2517v@gmail.com>
2023-04-24 20:54:01 +0700

    start menu

--- react-laravel/src/router.jsx
+++ react-laravel/src/router.jsx
@@ -1,31 +1,60 @@
-import { createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import Signup from "./views/Signup";
 import Login from "./views/Login";
 import Surveys from "./views/Surveys";
 import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
 
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
+
   {
-    path: '/',
-    element: <Dashboard />
-  },
-  {
-    path: '/surveys',
-    element: <Surveys />
+    path: "/",
+    element: <DefaultLayout/>,
+    children: [
+      /*
+      1 ая версия пути
+      {
+        path: '/',
+        element: <Dashboard/>
+      },
+      2 ая версия пути
+      {
+        path: '/',
+        element: <Navigate to="/dashboard" />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard/>
+      },*/
+      /*3 ая версия пути*/
+      {
+        path: '/dashboard',
+        element: <Navigate to="/" />
+      },
+      {
+        path: '/',
+        element: <Dashboard/>
+      },
+      {
+        path: '/surveys',
+        element: <Surveys/>
+      },
+    ]
   },
   {
     path: '/', //url будет /login
     //path: '/guest', //url будет /guest/login
-    element: <GuestLayout />,
+    element: <GuestLayout/>,
     children: [
       {
         path: 'signup',
-        element: <Signup />
+        element: <Signup/>
       },
       {
         path: 'login',
-        element: <Login />
+        element: <Login/>
       },
     ]
   },
--- react-laravel/src/views/Dashboard.jsx
+++ react-laravel/src/views/Dashboard.jsx
@@ -2,6 +2,17 @@ import React from "react";
 
 export default function Dashboard() {
   return (
-    <div>Дашборд</div>
+    <>
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Дашборд</h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+
+        </div>
+      </main>
+    </>
   )
 }
